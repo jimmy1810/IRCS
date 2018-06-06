509ba69d94bc08b372967e978fe46c70689c52e3
jimmy1810 <mittaljitesh1810@gmail.com>
2018-06-06 14:40:20 +0530

    second

--- ircs/models.py
+++ ircs/models.py
@@ -1,3 +1,99 @@
 from django.db import models
 
 # Create your models here.
+class HBsAg(models.Model):
+	SampleNo1=models.BooleanField(default=True)
+	SampleNo2=models.BooleanField(default=True)
+	SampleNo3=models.BooleanField(default=True)
+	SampleNo4=models.BooleanField(default=True)
+	SampleNo5=models.BooleanField(default=True)
+	AchievableScore=models.IntegerField(default=0)
+	YourScore=models.IntegerField(default=0)
+
+class Anti_HIV(models.Model):
+	SampleNo1=models.BooleanField(default=True)
+	SampleNo2=models.BooleanField(default=True)
+	SampleNo3=models.BooleanField(default=True)
+	SampleNo4=models.BooleanField(default=True)
+	SampleNo5=models.BooleanField(default=True)
+	AchievableScore=models.IntegerField(default=0)
+	YourScore=models.IntegerField(default=0)
+
+class Anti_HCV(models.Model):
+	SampleNo1=models.BooleanField(default=True)
+	SampleNo2=models.BooleanField(default=True)
+	SampleNo3=models.BooleanField(default=True)
+	SampleNo4=models.BooleanField(default=True)
+	SampleNo5=models.BooleanField(default=True)
+	AchievableScore=models.IntegerField(default=0)
+	YourScore=models.IntegerField(default=0)		
+
+class Syphilis(models.Model):
+	SampleNo1=models.BooleanField(default=True)
+	SampleNo2=models.BooleanField(default=True)
+	SampleNo3=models.BooleanField(default=True)
+	SampleNo4=models.BooleanField(default=True)
+	SampleNo5=models.BooleanField(default=True)
+	AchievableScore=models.IntegerField(default=0)
+	YourScore=models.IntegerField(default=0)
+
+class Malaria_Parasite(models.Model):
+	SampleNo6=models.BooleanField(default=True)
+	SampleNo7=models.BooleanField(default=True)
+	AchievableScore=models.IntegerField(default=0)
+	YourScore=models.IntegerField(default=0)
+
+class Haemoglobin(models.Model):
+	SampleNo6_Sat_or_Not_Sat=models.BooleanField(default=True)
+	SampleNo6_Mean_Value=models.IntegerField(default=0)
+	SampleNo6_Your_Result=models.IntegerField(default=0)
+	SampleNo6_Your_SDI_Value=models.IntegerField(default=0)
+	SampleNo7_Sat_or_Not_Sat=models.BooleanField(default=True)
+	SampleNo7_Mean_Value=models.IntegerField(default=0)
+	SampleNo7_Your_Result=models.IntegerField(default=0)
+	SampleNo7_Your_SDI_Value=models.IntegerField(default=0)
+	AchievableScore=models.IntegerField(default=0)
+	YourScore=models.IntegerField(default=0)
+
+class Direct_Anti_globin_Test(models.Model):
+	SampleNo6=models.BooleanField(default=True)
+	AchievableScore=models.IntegerField(default=0)
+	YourScore=models.IntegerField(default=0)
+
+class ABO_and_Rh_on_Patient(models.Model):
+	SampleNo6=models.BooleanField(default=True)
+	AchievableScore=models.IntegerField(default=0)
+	YourScore=models.IntegerField(default=0)
+
+class Sample_Identification_Scoring_on_Patient(models.Model):
+	SampleNo6=models.BooleanField(default=True)
+	AchievableScore=models.IntegerField(default=0)
+	YourScore=models.IntegerField(default=0)
+			
+class ABO_and_Rh_on_Donors(models.Model):
+	SampleNo7=models.BooleanField(default=True)
+	SampleNo8=models.BooleanField(default=True)
+	SampleNo9=models.BooleanField(default=True)
+	AchievableScore=models.IntegerField(default=0)
+	YourScore=models.IntegerField(default=0)
+
+class Sample_Identification_Scoring_on_Donors(models.Model):
+	SampleNo7=models.BooleanField(default=True)
+	SampleNo8=models.BooleanField(default=True)
+	SampleNo9=models.BooleanField(default=True)
+	AchievableScore=models.IntegerField(default=0)
+	YourScore=models.IntegerField(default=0)
+
+class Compatibility_Testing(models.Model):
+	SampleNo7_and_10=models.BooleanField(default=True)
+	SampleNo8_and_11=models.BooleanField(default=True)
+	SampleNo9_and_12=models.BooleanField(default=True)
+	AchievableScore=models.IntegerField(default=0)
+	YourScore=models.IntegerField(default=0)
+
+class Sample_Identification_Scoring(models.Model):
+	SampleNo7_and_10=models.BooleanField(default=True)
+	SampleNo8_and_11=models.BooleanField(default=True)
+	SampleNo9_and_12=models.BooleanField(default=True)
+	AchievableScore=models.IntegerField(default=0)
+	YourScore=models.IntegerField(default=0)					
--- mysite/settings.py
+++ mysite/settings.py
@@ -27,10 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
+    'ircs.apps.IrcsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
