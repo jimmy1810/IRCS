17f876ef7d627aea19ccfddf4cfa6c8a06264be5
bharat <malhanbharat6600@gmail.com>
2018-06-08 19:23:39 +0530

    Add files via upload

---
+++ eqas/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+
+class HIV16_1(models.Model):
+	absorbance = models.FloatField(default = 0.0)
+	cutoff_value = models.FloatField(default = 0.0)
+	cutoff_ratio = models.FloatField(default = 0.0)
+	test_interpretation = models.CharField(max_length = 12)
+
+class HIV16_2(models.Model):
+	absorbance = models.FloatField(default = 0.0)
+	cutoff_value = models.FloatField(default = 0.0)
+	cutoff_ratio = models.FloatField(default = 0.0)
+	test_interpretation = models.CharField(max_length = 12)
+
+class HIV16_3(models.Model):
+	absorbance = models.FloatField(default = 0.0)
+	cutoff_value = models.FloatField(default = 0.0)
+	cutoff_ratio = models.FloatField(default = 0.0)
+	test_interpretation = models.CharField(max_length = 12)
+
+class HIV16_4(models.Model):
+	absorbance = models.FloatField(default = 0.0)
+	cutoff_value = models.FloatField(default = 0.0)
+	cutoff_ratio = models.FloatField(default = 0.0)
+	test_interpretation = models.CharField(max_length = 12)
+
+class HIV16_5(models.Model):
+	absorbance = models.FloatField(default = 0.0)
+	cutoff_value = models.FloatField(default = 0.0)
+	cutoff_ratio = models.FloatField(default = 0.0)
+	test_interpretation = models.CharField(max_length = 12)
+
+class HIV16_6(models.Model):
+	absorbance = models.FloatField(default = 0.0)
+	cutoff_value = models.FloatField(default = 0.0)
+	cutoff_ratio = models.FloatField(default = 0.0)
+	test_interpretation = models.CharField(max_length = 12)
+
+class HIV16_7(models.Model):
+	absorbance = models.FloatField(default = 0.0)
+	cutoff_value = models.FloatField(default = 0.0)
+	cutoff_ratio = models.FloatField(default = 0.0)
+	test_interpretation = models.CharField(max_length = 12)
+
+class HIV16_8(models.Model):
+	absorbance = models.FloatField(default = 0.0)
+	cutoff_value = models.FloatField(default = 0.0)
+	cutoff_ratio = models.FloatField(default = 0.0)
+	test_interpretation = models.CharField(max_length = 12)
